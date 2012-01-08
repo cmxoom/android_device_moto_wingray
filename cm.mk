33a1baa78f6e5fffffced14a0f166e93dc57e5e3
Neil Perry <nperry@gmail.com>
2012-01-08 18:46:12 +0000

    change cm.mk around a little :/

--- cm.mk
+++ cm.mk
@@ -1,6 +1,3 @@
-# Release name
-PRODUCT_RELEASE_NAME := XOOM
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
@@ -10,8 +7,12 @@ $(call inherit-product, device/moto/wingray/full_wingray.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := wingray
 PRODUCT_NAME := cm_wingray
-PRODUCT_BRAND := Motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := Xoom
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wingray TARGET_DEVICE=tervigon BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT="motorola/tervigon/wingray:4.0.3/IML74K/190830:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.0.3 IML74K 190830 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=wingray BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT="motorola/tervigon/wingray:4.0.3/IML74K/190830:user/release-keys" PRIVATE_BUILD_DESC="tervigon-user 4.0.3 IML74K 190830 release-keys"
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := wingray
